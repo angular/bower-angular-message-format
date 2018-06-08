f35bb3830e94478d1534a948a179bf568d4a1375
jenkins <ci-bot@angularjs.org>
2018-06-08 06:57:51 -0700

    v1.7.1-build.5556+sha.05170bf

--- angular-message-format.js
+++ angular-message-format.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.7.0
+ * @license AngularJS v1.7.1-build.5556+sha.05170bf
  * (c) 2010-2018 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -1056,7 +1056,7 @@ var toJson;
 var $$stringify;
 
 var module = window['angular']['module']('ngMessageFormat', ['ng']);
-module['info']({ 'angularVersion': '1.7.0' });
+module['info']({ 'angularVersion': '1.7.1-build.5556+sha.05170bf' });
 module['factory']('$$messageFormat', $$MessageFormatFactory);
 module['config'](['$provide', function($provide) {
   $interpolateMinErr = window['angular']['$interpolateMinErr'];
